14badcc19c0e70cad8d9d2d1920a5db99e1d7e9c
jffryan <jffryan@gmail.com>
2019-09-23 13:16:46 -0500

    updated media queries

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,4 @@
-const nav = document.querySelector("#main_nav");
+const nav = document.querySelector("#main-nav");
 const topOfNav = nav.offsetTop;
 
 function fixNav() {
@@ -12,3 +12,27 @@ function fixNav() {
 }
 
 window.addEventListener("scroll", fixNav);
+
+const navSlide = () => {
+  const burger = document.querySelector(`.burger`);
+  const nav = document.querySelector(`.nav-links`);
+  const navLinks = document.querySelectorAll(`.nav-links li`);
+
+  //   Toggle Nav
+  burger.addEventListener(`click`, () => {
+    nav.classList.toggle(`nav-active`);
+    //   Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ``;
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
+          0.2}s`;
+      }
+    });
+    // Burger Animation
+    burger.classList.toggle(`toggle`);
+  });
+};
+
+navSlide();
